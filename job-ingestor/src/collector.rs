f46dcca1da9e71d79b92e6326abc575cbb70adc9
Aravind Nidadavolu <aravind@aravindn.com>
2025-06-13 14:27:16 -0700

    Add collector trait and factory to job-ingestor

---
+++ job-ingestor/src/collector.rs
@@ -0,0 +1,196 @@
+use std::{
+    collections::{HashMap, HashSet},
+    error::Error,
+    str::FromStr,
+};
+
+use async_trait::async_trait;
+
+/// Trait that defines behaviors for all job collectors
+///
+/// Each collector is responsible for fetching job postings
+/// from a source and providing a consistent interface to
+/// the ingestion engine
+#[async_trait]
+pub(crate) trait JobCollector: Send + Sync {
+    /// Executes job collection logic
+    async fn collect(&self) -> Result<(), Box<dyn Error>>;
+
+    /// Returns the string identifier of a collector
+    ///
+    /// Used to get a name for logging purposes
+    fn name(&self) -> &'static str;
+}
+
+/// Strongly typed identifier for Collectors
+///
+/// This enum is used to avoid using strings as keys
+/// during configuration
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+pub(crate) enum Collector {
+    /// Microsoft Careers site collector
+    Microsoft,
+
+    /// Google Careers site collector
+    Google,
+}
+
+/// Type alias for factory functions that return boxed [`JobCollector`]
+///
+/// Allows dynamic instantiation of collectors at runtime
+type CollectorFactory = Box<dyn Fn() -> Box<dyn JobCollector + Send + Sync>>;
+
+impl Collector {
+    /// Returns a lowercase string representation of a collector
+    ///
+    /// Useful for logging
+    #[allow(dead_code)]
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Collector::Microsoft => "microsoft",
+            Collector::Google => "google",
+        }
+    }
+
+    /// Parses a list of collector names to [`Collector`] variants
+    ///
+    /// # Arguments
+    /// * `config` - A vector of collector keys as strings (Example: "microsoft", "google")
+    ///
+    /// # Returns
+    /// A vector of [`Collector`] values
+    ///
+    /// # Errors
+    /// Prints each failed key to stderr
+    pub fn load_collector_config(config: Vec<String>) -> Vec<Collector> {
+        config
+            .into_iter()
+            .map(|s| s.to_lowercase())
+            .fold(HashSet::new(), |mut collector_set, s| {
+                match s.parse::<Collector>() {
+                    Ok(k) => {
+                        collector_set.insert(k);
+                    }
+                    Err(e) => eprintln!("{e}"),
+                }
+                collector_set
+            })
+            .into_iter()
+            .collect()
+    }
+
+    /// Builds a map of all available Collector factory functions
+    ///
+    /// # Returns
+    /// A map of [`Collector`] values to the corresponding factory functions
+    pub fn build_factory_map() -> HashMap<Collector, CollectorFactory> {
+        // Insert a mapping here
+        // Example:
+        // factory_map.insert(
+        //     Collector::Microsoft,
+        //     Box::new(|| Box::new(MicrosoftCollector::new())),
+        // );
+        HashMap::new()
+    }
+}
+
+impl FromStr for Collector {
+    type Err = String;
+
+    /// Parses a string into a [`Collector`] variant
+    ///
+    /// # Errors
+    /// Returns an error string for each invalid collector key
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "microsoft" => Ok(Collector::Microsoft),
+            "google" => Ok(Collector::Google),
+            _ => Err(format!("Invalid Collector Name: {}", s)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_as_str_microsoft() {
+        assert_eq!("microsoft", Collector::Microsoft.as_str())
+    }
+
+    #[test]
+    fn test_as_str_google() {
+        assert_eq!("google", Collector::Google.as_str())
+    }
+
+    #[test]
+    fn test_from_str_microsoft() {
+        let result = Collector::from_str("microsoft");
+        assert!(result.is_ok());
+        assert_eq!(Collector::Microsoft, result.unwrap())
+    }
+
+    #[test]
+    fn test_from_str_google() {
+        let result = Collector::from_str("google");
+        assert!(result.is_ok());
+        assert_eq!(Collector::Google, result.unwrap());
+    }
+
+    #[test]
+    fn test_from_str_invalid_value() {
+        assert!(Collector::from_str("invalid").is_err())
+    }
+
+    #[test]
+    fn test_load_collector_config_valid_values() {
+        let config = vec![String::from("microsoft"), String::from("google")];
+        let expected: HashSet<_> = vec![Collector::Microsoft, Collector::Google]
+            .into_iter()
+            .collect();
+        let actual = Collector::load_collector_config(config)
+            .into_iter()
+            .collect();
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_load_collector_config_invalid_values() {
+        let invalid_config = vec![String::from("invalid")];
+        assert!(Collector::load_collector_config(invalid_config).is_empty())
+    }
+
+    #[test]
+    fn test_load_collector_config_mixed_values() {
+        let mixed_config = vec![String::from("microsoft"), String::from("invalid")];
+        let expected = vec![Collector::Microsoft];
+        let actual = Collector::load_collector_config(mixed_config);
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_load_collector_config_empty() {
+        assert!(Collector::load_collector_config(Vec::new()).is_empty())
+    }
+
+    #[test]
+    fn test_load_collector_config_with_duplicates() {
+        let duplicate_config = vec![String::from("microsoft"), String::from("microsoft")];
+        let expected = vec![Collector::Microsoft];
+        assert_eq!(expected, Collector::load_collector_config(duplicate_config))
+    }
+
+    #[test]
+    fn test_load_collector_config_case_insensitivity() {
+        let duplicate_config = vec![String::from("micRosOft")];
+        let expected = vec![Collector::Microsoft];
+        assert_eq!(expected, Collector::load_collector_config(duplicate_config))
+    }
+
+    #[test]
+    fn test_build_factory_map() {
+        assert!(Collector::build_factory_map().is_empty())
+    }
+}
--- job-ingestor/src/main.rs
+++ job-ingestor/src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+use futures::{StreamExt, stream::FuturesUnordered};
+use tokio::signal::unix::{SignalKind, signal};
+use tokio::time::{Duration, sleep};
+
+use crate::collector::Collector;
+
+mod collector;
+
+async fn run_collector_loop() {
+    let config = vec![String::from("microsoft")];
+
+    let enabled_collectors = Collector::load_collector_config(config);
+    let factory_map = Collector::build_factory_map();
+
+    loop {
+        let mut tasks = FuturesUnordered::new();
+
+        for collector in enabled_collectors.iter() {
+            if let Some(factory) = factory_map.get(collector) {
+                let collector = factory();
+
+                tasks.push(async move {
+                    match collector.collect().await {
+                        Ok(_) => println!("INFO: Collector {} finished", collector.name()),
+                        Err(e) => eprintln!("ERROR: Collector {} failed: {}", collector.name(), e),
+                    }
+                });
+            }
+        }
+
+        while tasks.next().await.is_some() {}
+
+        sleep(Duration::from_secs(5 * 60)).await;
+    }
+}
+
+async fn shutdown_signal() {
+    let mut sigterm = signal(SignalKind::terminate()).unwrap();
+
+    tokio::select! {
+        _ = sigterm.recv() => {
+            println!("INFO: Received SIGTERM");
+        }
+        _ = tokio::signal::ctrl_c() => {
+            println!("INFO: Received SIGINT");
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    println!("job-ingestor started. Press Ctrl + C to exit");
+
+    tokio::select! {
+        _ = run_collector_loop() => {
+            println!("INFO: Collector loop exited");
+        }
+        _ = shutdown_signal() =>  {
+            println!("INFO: Shutdown signal received");
+        }
+    }
 }
